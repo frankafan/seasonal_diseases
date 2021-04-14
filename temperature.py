18f842409685e45badab9f585a0618a33268d902
Frank <frank_fan_1201@outlook.com>
2021-04-14 18:49:16 -0400

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -3,7 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 FILE = 'west_climate_region_temp.csv'
-RANGE = [8, -12]
+RANGE = [12, -13]  # from 1998-01 to 2014-12
 data = pd.read_csv(FILE, skiprows=4)[RANGE[0]:RANGE[1]]
 
-print(data)
+temperature = data['Value'].to_numpy()
+month = np.arange(len(temperature))
+
+plt.figure()
+plt.plot(month, temperature)
+plt.show()
