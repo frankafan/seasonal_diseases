b4dd67a9e613b6a1e082bc4dbec0db79e5195668
Frank <frank_fan_1201@outlook.com>
2021-04-15 01:23:55 -0400

    Update cross_correlation.py

--- cross_correlation.py
+++ cross_correlation.py
@@ -1,8 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from influenza import total_patients
+from data_by_region import data_by_region
 from temperature import temperature, month
 
+REGION = 'Pacific'
+PROPERTY = 'TOTAL PATIENTS'
+RANGE = [14, -3]  # from 1998-01 to 2014-12
+
+total_patients = data_by_region(REGION, PROPERTY, RANGE)
 patients_monthly = total_patients[::4][:-18]
 temperature_change = np.zeros(len(temperature))
 patient_change = np.zeros(len(temperature))
@@ -16,5 +21,5 @@ plt.figure()
 plt.plot(month, temperature_change)
 plt.plot(month, patient_change)
 plt.figure()
-plt.xcorr(patient_change, -temperature_change)
+plt.xcorr(patient_change, -temperature_change, maxlags=None)
 plt.show()
