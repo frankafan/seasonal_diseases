5b2a4989cc3ad20bc410bfc3af688422561ff63a
Frank <frank_fan_1201@outlook.com>
2021-04-15 09:25:33 -0400

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-FILE = 'west_climate_region_temp.csv'
+FILE = 'west_climate_region_temp.csv'  # https://www.ncdc.noaa.gov/cag/national/time-series
 RANGE = [12, -13]  # from 1998-01 to 2014-12
 data = pd.read_csv(FILE, skiprows=4)[RANGE[0]:RANGE[1]]
 
